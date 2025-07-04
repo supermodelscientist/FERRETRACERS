e487a79b9f71260c07bc872faeab1f29db368124
supermodelscientist <d3morris@gmail.com>
2025-07-04 16:20:21 -0400

    ok

--- scripts/button_head_down.gd
+++ scripts/button_head_down.gd
@@ -3,5 +3,9 @@ extends Button
 
 func _on_button_down() -> void:
 	Globals.head_number -= 1
+	var sc = Globals.head_number
+	if Globals.head_number<1:
+		sc = 1.0/(2-sc)
+	
 	var orig_transform = get_node("%Node3D").scale
-	get_node("%Node3D").scale.x*=(Globals.head_number/(Globals.head_number+1))
+	get_node("%Node3D").scale.x*=(sc/(sc+1))
--- scripts/button_head_up.gd
+++ scripts/button_head_up.gd
@@ -5,7 +5,13 @@ extends Button
 func _on_pressed() -> void:
 	var orig_transform = get_node("%Node3D").scale
 	Globals.head_number += 1
-	get_node("%Node3D").scale.x*=Globals.head_number/(Globals.head_number-1)
-	print(Globals.head_number/(Globals.head_number-1))
+	
+	var sc = Globals.head_number
+	if sc<1:
+		sc = 1.0/(2-sc)
+	if sc==1:
+		get_node("%Node3D").scale.x*=2
+	else:
+		get_node("%Node3D").scale.x*=sc/(sc-1)
 
 	
