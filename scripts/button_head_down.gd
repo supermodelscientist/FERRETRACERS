aff153a7364ce04fd5b219272ee1d2ab8dfbf930
supermodelscientist <d3morris@gmail.com>
2025-07-05 10:43:03 -0400

    wide head

--- scripts/button_head_down.gd
+++ scripts/button_head_down.gd
@@ -2,10 +2,11 @@ extends Button
 
 
 func _on_button_down() -> void:
-	Globals.head_number -= 1
+	Globals.head_number -= 1.0
+	var dive = get_node("%Node3D").get_node("%Mustelid V1").get_node("Empty/DEF_Halo2/Skeleton3D/Head1")
 	var sc = Globals.head_number
 	if Globals.head_number<1:
 		sc = 1.0/(2-sc)
 	
 	var orig_transform = get_node("%Node3D").scale
-	get_node("%Node3D").scale.x*=(sc/(sc+1))
+	get_node("%Node3D").scale.x*=(sc/(sc+1.0))
--- scripts/button_head_up.gd
+++ scripts/button_head_up.gd
@@ -4,14 +4,15 @@ extends Button
 
 func _on_pressed() -> void:
 	var orig_transform = get_node("%Node3D").scale
-	Globals.head_number += 1
+	var dive = get_node("%Node3D").get_node("%Mustelid V1").get_node("Empty/DEF_Halo2/Skeleton3D/Head1")
 	
+	Globals.head_number += 1.0
 	var sc = Globals.head_number
 	if sc<1:
 		sc = 1.0/(2-sc)
 	if sc==1:
-		get_node("%Node3D").scale.x*=2
+		dive.scale.x*=2.0
 	else:
-		get_node("%Node3D").scale.x*=sc/(sc-1)
+		dive.scale.x*=sc/(sc-1.0)
 
 	
